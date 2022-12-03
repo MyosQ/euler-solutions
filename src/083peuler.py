fc7656d3f5b15846f1782af813eb476ae565fea4
MyosQ <frejsundqvist@protonmail.com>
2022-12-03 23:15:24 +0100

    simplifying 83

--- src/083peuler.py
+++ src/083peuler.py
@@ -1,5 +1,3 @@
-INPUT_PATH = f"../input_files/081peuler.txt"
-
 # define class for one "cell" in the grid. Each cell has a value and a "min path" value
 class Cell:
     def __init__(self, value):
@@ -7,52 +5,45 @@ class Cell:
         self.min_path = -1  # -1 means not yet calculated
 
 def main():
-    arr = []
-    with open(INPUT_PATH, "r") as f:
-        lines = f.readlines()
-        r = 0
-        for line in lines:
+    with open(f"../input_files/081peuler.txt", "r") as f:
+        arr, r = [], 0
+        for line in f.readlines():
             arr.append([])
             for cell in line.strip().split(","):
                 arr[r].append(Cell(int(cell)))
             r += 1               
-    arr[0][0].min_path = arr[0][0].value
+    arr[0][0].min_path = arr[0][0].value # starting cell
+    num_rows, num_cols = len(arr), len(arr[0])
 
-    was_updated = False
     while True:
         was_updated = False
-        for r in range(len(arr)):
-            for c in range(len(arr[r])):
+        for r in range(num_rows):
+            for c in range(num_cols):
                 # check left
-                if c > 0:
-                    if arr[r][c-1].min_path != -1:
-                        if arr[r][c].min_path == -1 or arr[r][c-1].min_path + arr[r][c].value < arr[r][c].min_path:
-                            arr[r][c].min_path = arr[r][c-1].min_path + arr[r][c].value
-                            was_updated = True
+                if c > 0 and  arr[r][c-1].min_path != -1:
+                    if arr[r][c].min_path == -1 or arr[r][c-1].min_path + arr[r][c].value < arr[r][c].min_path:
+                        arr[r][c].min_path = arr[r][c-1].min_path + arr[r][c].value
+                        was_updated = True
                 # check up
-                if r > 0:
-                    if arr[r-1][c].min_path != -1:
-                        if arr[r][c].min_path == -1 or arr[r-1][c].min_path + arr[r][c].value < arr[r][c].min_path:
-                            arr[r][c].min_path = arr[r-1][c].min_path + arr[r][c].value
-                            was_updated = True
+                if r > 0 and arr[r-1][c].min_path != -1:
+                    if arr[r][c].min_path == -1 or arr[r-1][c].min_path + arr[r][c].value < arr[r][c].min_path:
+                        arr[r][c].min_path = arr[r-1][c].min_path + arr[r][c].value
+                        was_updated = True
                 # check right
-                if c < len(arr[r]) - 1:
-                    if arr[r][c+1].min_path != -1:
-                        if arr[r][c].min_path == -1 or arr[r][c+1].min_path + arr[r][c].value < arr[r][c].min_path:
-                            arr[r][c].min_path = arr[r][c+1].min_path + arr[r][c].value
-                            was_updated = True
+                if c + 1 < num_cols and arr[r][c+1].min_path != -1:
+                    if arr[r][c].min_path == -1 or arr[r][c+1].min_path + arr[r][c].value < arr[r][c].min_path:
+                        arr[r][c].min_path = arr[r][c+1].min_path + arr[r][c].value
+                        was_updated = True
                 # check down
-                if r < len(arr) - 1:
-                    if arr[r+1][c].min_path != -1:
-                        if arr[r][c].min_path == -1 or arr[r+1][c].min_path + arr[r][c].value < arr[r][c].min_path:
-                            arr[r][c].min_path = arr[r+1][c].min_path + arr[r][c].value
-                            was_updated = True
-                
+                if r + 1 < num_rows and arr[r+1][c].min_path != -1:
+                    if arr[r][c].min_path == -1 or arr[r+1][c].min_path + arr[r][c].value < arr[r][c].min_path:
+                        arr[r][c].min_path = arr[r+1][c].min_path + arr[r][c].value
+                        was_updated = True
+
         if not was_updated:
             break
 
-    print(arr[len(arr)-1][len(arr[len(arr)-1])-1].min_path)
-    return
+    print(arr[num_rows-1][num_cols-1].min_path)
 
 if __name__ == "__main__":
     main()
